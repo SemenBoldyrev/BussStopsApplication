4794709b3aecbcb3ba2e693616c5d835f0ce3087
Semen Boldyrev <233458958+SemenBoldyrev@users.noreply.github.com>
2026-01-15 19:28:11 +0200

    final changes

--- Frontend/frontend.js
+++ Frontend/frontend.js
@@ -21,6 +21,8 @@ const busStopLabel = document.getElementById("selected-bus-stop");
 const clearButton = document.getElementById("clear-button");
 const infoHead = document.getElementById("infoHead");
 
+const busIcon = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-bus-front-fill\" viewBox=\"0 0 16 16\"><path d=\"M16 7a1 1 0 0 1-1 1v3.5c0 .818-.393 1.544-1 2v2a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5V14H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2a2.5 2.5 0 0 1-1-2V8a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1V2.64C1 1.452 1.845.408 3.064.268A44 44 0 0 1 8 0c2.1 0 3.792.136 4.936.268C14.155.408 15 1.452 15 2.64V4a1 1 0 0 1 1 1zM3.552 3.22A43 43 0 0 1 8 3c1.837 0 3.353.107 4.448.22a.5.5 0 0 0 .104-.994A44 44 0 0 0 8 2c-1.876 0-3.426.109-4.552.226a.5.5 0 1 0 .104.994M8 4c-1.876 0-3.426.109-4.552.226A.5.5 0 0 0 3 4.723v3.554a.5.5 0 0 0 .448.497C4.574 8.891 6.124 9 8 9s3.426-.109 4.552-.226A.5.5 0 0 0 13 8.277V4.723a.5.5 0 0 0-.448-.497A44 44 0 0 0 8 4m-3 7a1 1 0 1 0-2 0 1 1 0 0 0 2 0m8 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0m-7 0a1 1 0 0 0 1 1h2a1 1 0 1 0 0-2H7a1 1 0 0 0-1 1\"/></svg>"
+
 var tRegions = ["Region A", "Region B", "Region C"];
 var tBusStops = ["Stop 1", "Stop 2", "Stop 3"];
 
@@ -44,6 +46,8 @@ regionButton.addEventListener("click", () => {
             MainVisController(true, false, false);
         }
     });
+    regionsList.style.display = "none";
+    busStopsList.style.display = "none";
 });
 
 busStopButton.addEventListener("click", () => {
@@ -66,6 +70,8 @@ busStopButton.addEventListener("click", () => {
             MainVisController(true, true, false);
         }
     });
+    regionsList.style.display = "none";
+    busStopsList.style.display = "none";
 });
 
 
@@ -244,13 +250,18 @@ async function UpdateBusButton()
     const data = await response.json();
     if (data.length == 0) 
     { 
-        const option = document.createElement("option");
-        option.style.cursor = "pointer";
-        option.style.border = "1px solid #ccc";
-        option.style.padding = "10px";
-        option.textContent = `No Buses Found`;
-        busListDiv.appendChild(option);
-        return; 
+        const label = document.createElement("label");
+        label.style.border = "1px solid #ccc";
+        label.style.padding = "10px";
+        label.style.width = "125px";
+        label.disabled = true;
+
+        label.classList.add("btn", "btn-primary", "mb-3", "pe-none");
+        label.ariaDisabled = "true";
+
+        label.innerHTML = `No Busses Found`;
+        busTimesDiv.appendChild(label);
+        return;
     }
 
     console.log("Updating bus buttons...");
@@ -273,17 +284,24 @@ async function UpdateBusTimes(routeLongName)
     const data = await response.json();
 
     if (data.length == 0) 
-        { 
-        const option = document.createElement("option");
-        option.style.border = "1px solid #ccc";
-        option.style.padding = "10px";
-        option.textContent = `No Times Found`;
-        busTimesDiv.appendChild(option);
+    { 
+        const label = document.createElement("label");
+        label.style.border = "1px solid #ccc";
+        label.style.padding = "10px";
+        label.style.width = "125px";
+        label.disabled = true;
+
+        label.classList.add("btn", "btn-primary", "mb-3", "pe-none");
+        label.ariaDisabled = "true";
+
+        label.innerHTML = `No Bus Times Found`;
+        busTimesDiv.appendChild(label);
         return; 
     }
 
     console.log("Updating bus times...");
-
+    //for confidencce
+    busTimesDiv.innerHTML = "";
     data.forEach(trip => {
         CreateBusTimesButton(trip);
     });
@@ -291,24 +309,35 @@ async function UpdateBusTimes(routeLongName)
 
 function CreateBusButton(route)
 {
-        const option = document.createElement("option");
-        option.onclick = () => {
-            busTimesDiv.innerHTML = "";
+        const button = document.createElement("button");
+        button.onclick = () => {
             console.log(`Bus route ${route.route_short_name} selected`);
             UpdateBusTimes(route.route_long_name);
         }
-        option.style.cursor = "pointer";
-        option.style.border = "1px solid #ccc";
-        option.style.padding = "10px";
-        option.textContent = `Bus: ${route.route_short_name}`;
-        busListDiv.appendChild(option);
+        button.style.cursor = "pointer";
+        button.style.border = "1px solid #ccc";
+        button.style.padding = "10px";
+        button.style.width = "125px";
+        //button.style.textAlign = "left";
+
+        button.classList.add("btn", "btn-primary", "mb-3");
+
+        button.innerHTML = `${busIcon} ${route.route_short_name}`;
+        busListDiv.appendChild(button);
 }
 
 function CreateBusTimesButton(stopTime)
 {
-    const option = document.createElement("option");
-    option.style.border = "1px solid #ccc";
-    option.style.padding = "10px";
-    option.textContent = `Arrival time: ${stopTime.arrival_time}`;
-    busTimesDiv.appendChild(option);
+    const label = document.createElement("label");
+    label.style.border = "1px solid #ccc";
+    label.style.padding = "10px";
+    label.style.width = "125px";
+    label.disabled = true;
+    //label.style.textAlign = "left";
+
+    label.classList.add("btn", "btn-primary", "mb-3", "pe-none");
+    label.ariaDisabled = "true";
+
+    label.innerHTML = `Arrival : ${stopTime.arrival_time}<br>Departure : ${stopTime.departure_time}`;
+    busTimesDiv.appendChild(label);
 }
